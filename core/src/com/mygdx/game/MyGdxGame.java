68b1f906683150bcb6c5f5d2ce6a187bbee840c7
KyleGass <tim.moenkemoeller@web.de>
2015-09-29 10:59:21 +0200

    committest

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -40,7 +40,7 @@ public class MyGdxGame implements ApplicationListener {
 	@Override
 	public void create() {
 		rotationSpeed = 0.5f;
-
+		System.out.println("committest");
 		map = new TmxMapLoader().load("exampletmx.tmx");
 
 		renderer = new OrthogonalTiledMapRenderer(map, unitScale);
